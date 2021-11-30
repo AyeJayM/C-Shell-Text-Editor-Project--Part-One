c89f7383724f03bbebb9477b73d5eefe43cb9ae6
Austin Martinez <austinm@cpp.edu>
2021-11-29 19:06:42 -0800

    Added function - lsh_read_line and added #includes for librarires

--- main.c
+++ main.c
@@ -2,6 +2,13 @@
 // CS 2600
 // Final Part 1 - Create a Shell in C Tutorial
 
+#include <sys/wait.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
 int main(int argc, char **argv)
 {
     //Load config files, if any.
@@ -30,5 +37,42 @@ void lsh_loop(void)
         free(args);
     }   while (status);
 
+}
 
+#define LSH_RL_BUFSIZE 1034
+char *lsh_read_line(void)
+{
+    int bufsize = LSH_RL_BUFSIZE;
+    int position = 0;
+    char *buffer = malloc(sizeof(char) * bufsize);
+    int c;
+
+    if (!buffer) {
+        fprintf(stderr, "lsh: allocation error\n");
+        exit(EXIT_FAILURE);
+    }
+
+    while (1) {
+        // Read a character
+        c = getchar();
+
+        // If we hit EOF, replace it with a null character and return.
+        if (c == EOF || c == '\n') {
+            buffer[position] = '\0';
+            return buffer;
+        }   else {
+            buffer[position] = c;
+        }
+        position++;
+
+        //If we exceeded the buffer, reallocate
+        if (position >= bufsize) {
+            bufsize += LSH_RL_BUFSIZE;
+            buffer = realloc(buffer, bufsize);
+            if (!buffer) {
+                fprintf(stderr, "lsh: allocation error\n");
+                exit(EXIT_FAILURE);
+            }
+        }
+    }
 }
