79d7ccc16808333f4fbe7591d600d74274d8096f
Austin Martinez <austinm@cpp.edu>
2021-11-29 18:24:59 -0800

    Created function - main

---
+++ main.c
@@ -0,0 +1,16 @@
+// Austin Martinez
+// CS 2600
+// Final Part 1 - Create a Shell in C Tutorial
+
+int main(int argc, char **argv)
+{
+    //Load config files, if any.
+
+    //Run command loop
+    lsh_loop();
+
+    //Perform any shutdown/cleanup.
+
+    return EXIT_SUCCESS;
+}
+
